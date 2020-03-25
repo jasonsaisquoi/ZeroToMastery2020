13ede120377efaee7e797f4dc59f6d6d0c6bef78
teknoformula <seraficaj@gmail.com>
2020-03-25 14:31:07 -0700

    complete first iteration of santa problem

--- 24/awesome-node/script.js
+++ 24/awesome-node/script.js
@@ -11,3 +11,26 @@ fs.readFile('./hello.txt', (err,data) => {
 //this ran first because this code is synchornous
 const file = fs.readFileSync('./hello.txt');
 console.log('#2', file.toString());
+
+
+//Append
+
+// fs.appendFile('./hello.txt', ' eat some ice cream', err => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
+
+//Write 
+
+// fs.writeFile('bye.txt', 'Sad to see you go!', err=> {
+//   if (err) {
+//     console.log(err);
+//   }
+// })
+
+// Delete
+fs.unlink('./bye.txt', err => {
+  if (err) console.log(err);
+});
+//bye.txt is deleted
